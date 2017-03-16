785b2f5cfd472eb0bfc1e7e1b61c7a7f76083716
Siqi Lin <siqilin@google.com>
2017-03-15 18:09:34 -0700

    pstore: Remove "-0" suffix from console-ramoops
    The Android framework expects last kmsg to be located at
    /sys/fs/pstore/console-ramoops when it collects crash metrics,
    so remove the "-%lld" suffix to maintain compatibility.
    Based on commit 0fb78957ffe0447a1072ec307ce342e5437c0791
    ("pstore: use scnprintf") from the Marlin tree.
    
    Bug: 36231027
    Change-Id: I3670edb12113b6962ad225ee56c48e1894171dab
    Signed-off-by: Siqi Lin <siqilin@google.com>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -385,12 +385,12 @@ int pstore_mkfile(struct pstore_record *record)
 			  record->compressed ? ".enc.z" : "");
 		break;
 	case PSTORE_TYPE_CONSOLE:
-		scnprintf(name, sizeof(name), "console-%s-%llu",
-			  record->psi->name, record->id);
+		scnprintf(name, sizeof(name), "console-%s",
+			  record->psi->name);
 		break;
 	case PSTORE_TYPE_FTRACE:
-		scnprintf(name, sizeof(name), "ftrace-%s-%llu",
-			  record->psi->name, record->id);
+		scnprintf(name, sizeof(name), "ftrace-%s",
+			  record->psi->name);
 		break;
 	case PSTORE_TYPE_MCE:
 		scnprintf(name, sizeof(name), "mce-%s-%llu",
