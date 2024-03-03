ebdf8f6b91f44ffeb1a0ab14e276a7e126b2bf47
Prabhat Maurya <prabhat@e.email>
2024-03-03 13:00:58 +0530

    add buildscript

---
+++ script.sh
@@ -0,0 +1,10 @@
+make O=out ARCH=arm64 msm8953-perf_defconfig
+
+PATH="../clang-azure/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
